bc87c19eaaf415c08ce9c3594a1be084ca0871d5
Lucian Pacurar <codesensedev@outlook.com>
2026-04-10 16:45:43 -0500

    remove unused

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'happy-dom',
+    environment: 'node',
     setupFiles: ['./test/setup.ts'],
     hookTimeout: 60000,
     testTimeout: 30000,
